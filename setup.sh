e73b3905bc22359d84f6c146f4e4722297756792
Matteo Beltrami <beltramimatteo01@gmail.com>
2024-04-30 14:55:01 +0200

    fix setup

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ chmod +x ./docker/build_docker.sh
 mkdir -p lab
 
 SCRIPT_CONTENT='#!/bin/bash
-docker run -it -v ./lab:/network_security_lab/lab network_security_lab_image'
+docker run -it -v lab:/network_security_lab/lab network_security_lab_image'
 
 echo "$SCRIPT_CONTENT" > start.sh
 chmod +x start.sh
