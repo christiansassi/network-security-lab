901f2eb4ecaff1cf8d73cbce2dd283bfd7793eac
christiansassi <21258355+christiansassi@users.noreply.github.com>
2024-04-30 10:15:15 +0200

    updates

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ docker pull kalilinux/kali-rolling
 chmod +x ./docker/build_docker.sh
 ./docker/build_docker.sh
 
-mkdir lab
+mkdir network_security_lab
 
 SCRIPT_CONTENT='#!/bin/bash
 
-docker run -it -v ./lab:/network_security_lab network_security_lab_image'
+docker run -it -v ./network_security_lab:/network_security_lab network_security_lab_image'
 
 echo "$SCRIPT_CONTENT" > start.sh
 chmod +x start.sh
