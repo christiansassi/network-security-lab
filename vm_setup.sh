83d3a74219b9ce914ecb64e6d1de9108218c8824
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2024-05-07 14:21:58 +0200

    updates

--- vm_setup.sh
+++ vm_setup.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-# Run with sudo
+GREEN="\033[1;32m"
+RESET="\033[0m"
+
+# Set netsec user as root
+su root -c '
+if [ $(sudo cat /etc/sudoers | grep -q "netsec ALL=(ALL:ALL) ALL" ; echo $?) -eq 1]
+then
+    echo "netsec ALL=(ALL:ALL) ALL" >> /etc/sudoers
+fi
+'
+
+# Run without sudo
 if [ "$(id -u)" -eq 0 ]; then
     echo "Do not run as root!" >&2
     exit 1
 fi
 
 # Instal system dependencies
-echo -e "\033[1;32mInstalling dependencies...\033[0m"
+echo -e "${GREEN}Installing dependencies...${RESET}"
 sudo apt update
 sudo apt install -y git libnl-3-dev libnl-genl-3-dev pkg-config libssl-dev net-tools git sysfsutils virtualenv wireshark arping openvswitch-switch
 
 # Install mininet
-echo -e "\033[1;32mInstalling mininet...\033[0m"
+echo -e "${GREEN}Installing mininet...${RESET}"
 sudo mininet-wifi/util/install.sh -Wln3fv
 
 # Patch krackattacks
-echo -e "\033[1;32mPatching krackattacks scripts...\033[0m"
+echo -e "${GREEN}Patching krackattacks scripts...${RESET}"
 new_interface="ap1-wlan0"
 sed -i "s/interface=.*/interface=$new_interface/" krackattacks-scripts/hostapd/hostapd.conf
 
@@ -24,7 +35,7 @@ new_line='		subprocess.call(["iw", self.nic_mon, "del"])'
 sed -i "/if self.sock_eth: self.sock_eth.close()/a\\$new_line" krackattacks-scripts/krackattack/krack-test-client.py
 
 # Setup krackattacks
-echo -e "\033[1;32mSetup krackattacks...\033[0m"
+echo -e "${GREEN}Setup krackattacks...${RESET}"
 git clone git://w1.fi/hostap.git krackattacks-scripts/hostap-wpa_supplicant-2.3
 git clone git://w1.fi/hostap.git krackattacks-scripts/hostap-wpa_supplicant-2.5
 
@@ -34,13 +45,14 @@ cp .config krackattacks-scripts/hostap-wpa_supplicant-2.5/wpa_supplicant/.config
 
 echo "src/common/attacks.h" > krackattacks-scripts/attacks.h
 
-echo -e "\033[1;32mRunning build.sh...\033[0m"
+echo -e "${GREEN}Running build.sh...${RESET}"
 (cd ./krackattacks-scripts/krackattack/ && ./build.sh)
 
-echo -e "\033[1;32mRunning pysetup.sh...\033[0m"
+echo -e "${GREEN}Running pysetup.sh...${RESET}"
 (cd ./krackattacks-scripts/krackattack/ && ./pysetup.sh)
 
+echo -e "${GREEN}Running disable-hwcrypto.sh...${RESET}"
 sudo ./krackattacks-scripts/krackattack/disable-hwcrypto.sh
 
 # Reboot
-echo -e "\033[1;32mReboot to apply changes\033[0m"
+echo -e "${GREEN}Reboot your computer to apply the changes${RESET}"
