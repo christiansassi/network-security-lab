89585873de17154c08e1432a5ad60f438fb8833d
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2024-05-08 10:06:45 +0200

    fix

--- labs/lab-0/start.sh
+++ labs/lab-0/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-width=$(tput cols)
-height=$(tput lines)
+dimensions=$(xdpyinfo | awk '/dimensions:/ {print $2}')
+width=$(echo "$dimensions" | cut -d 'x' -f 1)
+height=$(echo "$dimensions" | cut -d 'x' -f 2)
 
 xterm -geometry "$((width / 2))x$((height / 2))+0+0" -e "python3 client.py" &
 xterm -geometry "$((width / 2))x$((height / 2))+0-0" -e "python3 server.py" &
