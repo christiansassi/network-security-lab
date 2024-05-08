3dc0b23e0df879c5790e107b6d67d05fad747056
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2024-05-08 10:18:32 +0200

    fixed start.sh of lab-0

--- labs/lab-0/start.sh
+++ labs/lab-0/start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-dimensions=$(xdpyinfo | awk '/dimensions:/ {print $2}')
-width=$(echo "$dimensions" | cut -d 'x' -f 1)
-height=$(echo "$dimensions" | cut -d 'x' -f 2)
-
-xterm -geometry "$((width / 2))x$((height / 2))+0+0" -e "python3 client.py" &
-xterm -geometry "$((width / 2))x$((height / 2))+0-0" -e "python3 server.py" &
-xterm -geometry "$((width / 2))x$((height / 2))-0+0" -e "python3 mitm.py" &
+xterm -geometry "80x20+0+0" -e "python3 client.py" &
+xterm -geometry "80x20+0-0" -e "python3 server.py" &
+xterm -geometry "80x20-0+0" -e "python3 mitm.py" &
 
 wait
