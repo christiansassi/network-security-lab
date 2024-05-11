bd56f15bf89aa9cf575ff4a1a1b555b09bf42940
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2024-05-11 18:43:27 +0200

    changed win size

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo xterm -maximized -e "chmod +x ./labs/lab.sh ; ./labs/lab.sh"
+sudo xterm -T "Network Security Lab" -geometry "125x40+0+0" -e "chmod +x ./labs/lab.sh ; ./labs/lab.sh"
